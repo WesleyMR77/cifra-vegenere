0a08dee6e76d9a7a7f044035c42fed1a4b032eaf
WesleyMR77 <wesley.rodrigues@estudante.ifms.edu>
2019-05-17 15:28:56 -0300

    código terminado

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
 let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-function cript() {
+function crip() {
     let cript = document.getElementById("message").value.toLowerCase()
-    let key = document.getElementById("key").value.toLowerCase()
+    let key = document.getElementById("key").value.toLowerCase() // pega elementos
     stop = 0
 
-    if (cript != "" && key != "") {
-        cript = removeAcento(cript)
+    if (cript != "" && key != "") { //verifica se não possui nada
+        cript = removeAccent(cript)
         r = cript.split(" ")
-        key = removeAcento(key)
-        key = trim(key)
+        key = removeAccent(key) //
+        key = putSpace(key)
+        stop = verKey(stop, key)
 
-        stop = verKey(stop, key) 
-
-        if (stop == 0) {
+        if (stop == 0) { //verifica se roda
             var j = 0
             v = []
-            for (f = 0; f < r.length; f++) {
+            for (let f = 0; f < r.length; f++) { //verifica a chave
                 cript = r[f]
                 for (i = 0; i < cript.length; i++) { //até palavra acabar
                     var pV = 0
                     while (true) {
-                        if (alphabet.includes(cript.charAt(i)) === false) {
-                            console.log(alphabet.includes(cript.charAt(i)))
+                        if (alphabet.includes(cript.charAt(i)) === false) { //adiciona elemento que não criptografa
+                            v.push(cript.charAt(i))
                             break
                         }
                         if (cript.charAt(i) == alphabet[pV]) { // encontrar colocação da palavra no vetor 
 
                             var pK = 0
                             while (true) {
-                                if (key.charAt(j) == alphabet[pK]) { //verifica a chave
+                                if (key.charAt(j) == alphabet[pK]) { // calculo para adicionar a letra com criptografia  
                                     if (pK + pV > 25) {
                                         v.push(alphabet[pK + pV - 26])
                                     }
@@ -60,13 +59,10 @@ function cript() {
             document.getElementById("result").innerHTML = "Insira corretamente a chave"
         }
     }
-    else{
+    else {
         document.getElementById("result").innerHTML = ("Preencha os Campos")
-   }
+    }
 }
-
-
-
 
 function descript() {
     let message = document.getElementById("message").value.toLowerCase()
@@ -74,82 +70,77 @@ function descript() {
     stop = 0
 
     if (message != "" && key != "") {
-        message = removeAcento(message)
+        message = removeAccent(message)
         r = message.split(" ")
-        console.log(r[0], r[1])
-        key = removeAcento(key)
-        key = trim(key)
-        console.log("a chave " + key)
+        key = removeAccent(key)
+        key = putSpace(key)
 
-        stop = verKey(stop, key)   
+        stop = verKey(stop, key)
 
         if (stop == 0) {
-        var j = 0
-        v = []
-        for (f = 0; f < r.length; f++) {
-            cript = r[f]
-            for (i = 0; i < cript.length; i++) { //até palavra acabar
-                var pV = 0
-                while (true) {
-                    if (alphabet.includes(cript.charAt(i)) === false) {
-                        console.log(alphabet.includes(cript.charAt(i)))
-                        break
-                    }
-                    if (cript.charAt(i) == alphabet[pV]) { // encontrar colocação da palavra no vetor 
-                        var pK = 0
-                        while (true) {
-                            if (key.charAt(j) == alphabet[pK]) { //verifica a chave             
-                                if (pV - pK < 0) {
-                                    v.push(alphabet[pV - pK + 26])
-                                }
-                                if (pV - pK >= 0) {
-                                    v.push(alphabet[pV - pK])
-                                }
-
-                                if (j < key.length) {
-                                    j += 1
-                                }
-                                if (j >= key.length) {
-                                    j = 0
-                                }
-                                break
-                            }
-                            if (alphabet.includes(key.charAt(j)) === false) {
-                                console.log(alphabet.includes(key.charAt(j)))
-                                j++
-                            }
-                            pK += 1
+            var j = 0
+            v = []
+            for (f = 0; f < r.length; f++) {
+                cript = r[f]
+                for (i = 0; i < cript.length; i++) { //até palavra acabar
+                    var pV = 0
+                    while (true) {
+                        if (alphabet.includes(cript.charAt(i)) === false) { //adiciona elemento que não criptografa
+                            v.push(cript.charAt(i))
+                            break
                         }
-                        break
+                        if (cript.charAt(i) == alphabet[pV]) { // encontrar colocação da palavra no vetor 
+                            var pK = 0
+                            while (true) {
+                                if (key.charAt(j) == alphabet[pK]) { //verifica a chave             
+                                    if (pV - pK < 0) {
+                                        v.push(alphabet[pV - pK + 26])
+                                    }
+                                    if (pV - pK >= 0) {
+                                        v.push(alphabet[pV - pK])
+                                    }
+
+                                    if (j < key.length) {
+                                        j += 1
+                                    }
+                                    if (j >= key.length) {
+                                        j = 0
+                                    }
+                                    break
+                                }
+                                if (alphabet.includes(key.charAt(j)) === false) {
+                                    j++
+                                }
+                                pK += 1
+                            }
+                            break
+                        }
+                        pV += 1
                     }
-                    pV += 1
                 }
+                v.push(" ") // adiciona espaço
             }
-            v.push(" ")
+            document.getElementById("result").innerHTML = v.join("")
         }
-        document.getElementById("result").innerHTML = v.join("")
-    }
         else {
             document.getElementById("result").innerHTML = "Insira corretamente a chave"
         }
     }
-    else{
+    else {
         document.getElementById("result").innerHTML = ("Preencha os Campos")
-   }
+    }
 }
 
-function verKey(stop,key){
+function verKey(stop, key) { //verifica chave
     for (let o = 0; o < key.length; o++) {
         if (alphabet.includes(key.charAt(o)) === false) {
-            console.log(alphabet.includes(key.charAt(o)))
             stop = 1
         }
     }
     return stop
 }
 
-
-function removeAcento(cript) {
+function removeAccent(cript) { //funçaõ tira acento
     cript = cript.replace(new RegExp('[ÁÀÂÃ]', 'gi'), 'a');
     cript = cript.replace(new RegExp('[ÉÈÊ]', 'gi'), 'e');
     cript = cript.replace(new RegExp('[ÍÌÎ]', 'gi'), 'i');
@@ -159,8 +150,7 @@ function removeAcento(cript) {
     return cript;
 }
 
-
-function trim(key) {
+function putSpace(key) { //adiciona espaço
     for (cont = 0; cont < key.length; cont++) {
         key = key.replace(" ", "")
     }
